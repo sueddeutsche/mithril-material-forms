421203e09b42eb70514d05d01f5ff2a53659cb56
Sascha Goldhofer <post@saschagoldhofer.de>
2017-03-04 11:02:57 +0100

    Add basic forms

---
+++ components/input/index.js
@@ -0,0 +1,17 @@
+const m = require("mithril");
+
+
+module.exports = {
+    view(vnode) {
+        return m("input.mmf-input",
+            {
+                id: vnode.attrs.id,
+                type: vnode.attrs.type,
+                value: vnode.attrs.value,
+                onchange: m.withAttr("value", vnode.attrs.onChange),
+                onfocus: vnode.attrs.onFocus,
+                onblur: vnode.attrs.onBlur
+            }
+        );
+    }
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
     button: require("./components/button"),
+    checkbox: require("./components/checkbox"),
+    input: require("./components/input"),
     select: require("./components/select")
 };
