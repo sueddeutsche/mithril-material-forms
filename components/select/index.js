e4b0dd54e02e79854a163c9f20921a81c0573820
Sascha Goldhofer <post@saschagoldhofer.de>
2017-08-24 09:11:30 +0200

    Documentation

--- components/select/index.js
+++ components/select/index.js
@@ -10,6 +10,8 @@ module.exports = {
                 "class": vnode.attrs.class,
                 onfocus: vnode.attrs.onfocus,
                 onblur: vnode.attrs.onblur,
+                // @reminder will always be string, which must be specified in json-schema or else datatype must
+                // be passed to select-component
                 onchange: m.withAttr("value", vnode.attrs.onchange)
             },
             vnode.attrs.options.map((value) => {
