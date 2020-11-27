626c6ac462098e59ac91d5ee726400b55edf7e1b
= <verena.schlott@netlight.com>
2020-11-27 13:03:47 +0100

    Updated dist

--- distmodule/components/select/index.js
+++ distmodule/components/select/index.js
@@ -1,13 +1,22 @@
 import m from "mithril";
 const isOptionValue = (option) => option && option.value !== undefined;
+const transformOptions = (options) => {
+    return options.map(option => typeof option === "string" ? { value: option } : option);
+};
 export default {
     view(vnode) {
+        var _a;
         const { attrs } = vnode;
         const { theme = "the-default" } = vnode.attrs;
+        const option = (_a = transformOptions(attrs.options)) === null || _a === void 0 ? void 0 : _a.find(o => o.value === attrs.value);
+        const activeClass = attrs.disabled === true ? "is-disabled" : "is-enabled";
         return m(".mmf-select__wrapper", {
-            "class": `${theme} ${attrs.disabled === true ? "is-disabled" : "is-enabled"}`,
+            "class": `${theme} ${activeClass} ${(option === null || option === void 0 ? void 0 : option.color) ? "with-color" : ""}`,
             oncreate: _vnode => (this.$wrapper = _vnode.dom)
-        }, m("select.mmf-select", {
+        }, m("span.select-icon", {
+            style: `background-color: ${option === null || option === void 0 ? void 0 : option.color}`,
+            oncreate: _vnode => (this.$icon = _vnode.dom)
+        }), m("select.mmf-select", {
             "data-id": attrs.id,
             value: attrs.value,
             disabled: attrs.disabled,
@@ -20,9 +29,14 @@ export default {
                 this.$wrapper && this.$wrapper.classList.remove("has-focus");
                 attrs.onblur && attrs.onblur(vnode);
             },
-            // @reminder will always be string, which must be specified in json-schema or else datatype must
-            // be passed to select-component
-            onchange: e => attrs.onchange(e.target.value)
+            onchange: (e) => {
+                var _a;
+                const option = (_a = transformOptions(attrs.options)) === null || _a === void 0 ? void 0 : _a.find(o => o.value === e.target.value);
+                this.$wrapper.classList.toggle("with-color", (option === null || option === void 0 ? void 0 : option.color) != null);
+                this.$icon.style.setProperty("background-color", option === null || option === void 0 ? void 0 : option.color);
+                if (attrs.onchange)
+                    attrs.onchange(e.target.value);
+            }
         }, attrs.options.map(value => {
             if (isOptionValue(value)) {
                 // value must be a string or else is discarded
