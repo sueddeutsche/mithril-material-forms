9beff078b42255d1b7087fb001b2247f739ba2db
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2020-12-30 15:33:01 +0100

    add: material theme support in query-list form

--- components/querylistform/index.ts
+++ components/querylistform/index.ts
@@ -7,8 +7,40 @@ import { DefaultFormAttrs, THEME_DEFAULT } from "../types";
 
 export type Attrs = DefaultFormAttrs & QueryListAttrs & LabelAttrs;
 
+export type State = {
+    $form: HTMLElement;
+    onfocus(): void;
+    onblur(value: string): void;
+    hasFocus(): boolean;
+}
 
 export default {
+
+    oncreate(vnode) {
+        this.$form = vnode.dom as HTMLElement;
+    },
+
+    updateClasses(value) {
+        const hasValue = value !== "";
+        this.$form.classList.remove(hasValue ? "is-empty" : "not-empty");
+        this.$form.classList.add(hasValue === false ? "is-empty" : "not-empty");
+    },
+
+    onfocus() {
+        this.$form.classList.add("has-focus");
+        this.$form.classList.remove("no-focus");
+    },
+
+    onblur(value) {
+        this.$form.classList.add("no-focus");
+        this.$form.classList.remove("has-focus");
+        this.updateClasses(value);
+    },
+
+    hasFocus() {
+        return this.$form && this.$form.classList.contains("has-focus");
+    },
+
     view(vnode) {
         const attrs = {
             id: null,
@@ -16,20 +48,36 @@ export default {
             errors: [],
             invertOrder: false,
             description: "",
+            onblur: Function.prototype,
+            onfocus: Function.prototype,
+            onchange: Function.prototype,
             ...vnode.attrs
         };
 
+        const focusClass = this.hasFocus() ? "has-focus" : "no-focus";
+        const errorClass = getErrorClass(attrs.errors);
+        const emptyClass = attrs.value === "" ? "is-empty" : "not-empty";
+
         return m(`.mmf-form.mmf-form--query-list.is-${attrs.disabled ? "disabled" : "enabled"}`,
             {
-                "class": `${attrs.theme ?? THEME_DEFAULT} ${getErrorClass(attrs.errors)}`
+                "class": `${focusClass} ${errorClass} ${emptyClass} ${attrs.theme ?? THEME_DEFAULT}`
             },
             m(Label,
                 {
-                    "class": "mmf-grow-2",
                     ...attrs,
                     invertOrder: attrs.invertOrder
                 },
-                m(QueryList, attrs)
+                m(QueryList, {
+                    ...attrs,
+                    onfocus: e => {
+                        this.onfocus();
+                        attrs.onfocus && attrs.onfocus(e);
+                    },
+                    onblur: e => {
+                        this.onblur(e.target.value);
+                        attrs.onblur && attrs.onblur(e);
+                    }
+                })
             ),
             m(Errors, attrs),
             attrs.description && m(".mmf-meta", attrs.description),
@@ -37,5 +85,4 @@ export default {
         );
     }
 
-} as m.Component<Attrs>;
-
+} as m.Component<Attrs, State>;
