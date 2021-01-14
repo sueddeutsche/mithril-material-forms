c7c342723d619d9387f52a5fdceaae55e5fb3225
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2021-01-14 17:37:43 +0100

    Bump version to 4.8.1

--- distmodule/components/querylist/index.js
+++ distmodule/components/querylist/index.js
@@ -18,13 +18,13 @@ export default {
         this.list = await this.getSuggestions(this.value);
         if (this.showCurrentInput) {
             if (this.currentInputDescription) {
-                this.list.unshift({
-                    [DESCRIPTION_PROP]: this.currentInputDescription,
-                    [this.valueProp]: this.value, class: "is-value"
-                });
+                this.list = [{
+                        [DESCRIPTION_PROP]: this.currentInputDescription,
+                        [this.valueProp]: this.value, class: "is-value"
+                    }, ...this.list];
             }
             else {
-                this.list.unshift({ [this.valueProp]: this.value, class: "is-value" });
+                this.list = [{ [this.valueProp]: this.value, class: "is-value" }, ...this.list];
             }
         }
         this.updateCompletions();
